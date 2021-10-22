a53646b41ad45358729de362ec9cc809bc664172
sofia <sofimrtn@gmail.com>
2021-10-22 20:16:41 +0200

    delegados para mostrar mensajes continuamente

---
+++ PBL_Grupo1/GestionDatosEstacion1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL_Grupo1
+{
+    class GestionDatosEstacion1
+    {
+        private delegadoMensajes imprimirMensajeRecibido;
+
+        public GestionDatosEstacion1(delegadoMensajes _datos)
+        {
+            imprimirMensajeRecibido = _datos;
+
+        }
+
+        public void procesar(byte[] datos, int dim)
+        {
+            imprimirMensajeRecibido(datos, dim);
+        }
+    }
+}
